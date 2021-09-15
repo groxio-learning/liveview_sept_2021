7f10885474b2be2338bcdfdad67c55628f23ea69
Jason Jones <jason@brucejones.biz>
2021-09-15 14:00:14 -0400

    refactor inc/dec

--- echo/lib/echo/map/math.ex
+++ echo/lib/echo/map/math.ex
@@ -1,13 +1,12 @@
 defmodule Echo.Map.Math do
-
   def initial, do: 0
 
   def inc(n), do: n + 1
 
   def dec(n), do: n - 1
 
+  # converter function
   def message(n) do
     "The treasure is at #{n}."
   end
-
 end
--- echo/lib/echo_web/live/treasure_live.ex
+++ echo/lib/echo_web/live/treasure_live.ex
@@ -2,22 +2,30 @@ defmodule EchoWeb.TreasureLive do
   use EchoWeb, :live_view
   alias Echo.Map.Math
 
+  # the socket is the state for LiveView
 
+  # called by genserver upon init.
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, steps: Math.initial)}
+    {:ok, assign(socket, steps: Math.initial())}
   end
 
-  def handle_event("inc", _value, socket ) do
-    increased_number = Math.inc(socket.assigns.steps)
-    {:noreply, assign(socket, steps: increased_number)}
+  # def render(assigns) if no render function, then load the template.
+
+  # javascript on the client side sends message through socket with
+  # "inc" as event type.
+  def handle_event("inc", _value, socket) do
+    {:noreply, inc(socket)}
   end
 
-
-  def handle_event("dec", _value, socket ) do
-    decreased_number = Math.dec(socket.assigns.steps)
-    {:noreply, assign(socket, steps: decreased_number)}
+  def handle_event("dec", _value, socket) do
+    {:noreply, dec(socket)}
   end
-  
 
+  defp inc(%{assigns: %{steps: n}} = socket) do
+    assign(socket, steps: Math.inc(n))
+  end
 
+  defp dec(%{assigns: %{steps: n}} = socket) do
+    assign(socket, steps: Math.dec(n))
+  end
 end
