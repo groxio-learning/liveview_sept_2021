7eb532a9496a4b71dea8e29ff797539ffb6504c2
Kimberly.Brooke.Johnson <siestakimberly@gmail.com>
2021-09-15 13:55:50 -0600

    we create a scaffold

--- echo/priv/repo/seeds.exs
+++ echo/priv/repo/seeds.exs
@@ -9,3 +9,30 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Echo.Library
+
+Library.create_passage(%{
+  name: "it",
+  phrase: "have you tried turning it on and off again?",
+  steps: 3})
+
+Library.create_passage(%{
+  name: "gump",
+  phrase: "life is like a box of chocolates, you never know what you are going to get.",
+  steps: 4})
+
+Library.create_passage(%{
+  name: "lincoln",
+  phrase: "four score and seven years ago",
+  steps: 6})
+
+Library.create_passage(%{
+  name: "incredibles",
+  phrase: "I am your wife, I am the greatest good you are every going to get!",
+  steps: 2})
+
+Library.create_passage(%{
+  name: "strikes-back",
+  phrase: "do or do not, there is no try",
+  steps: 2})
