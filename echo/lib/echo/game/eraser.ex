495731c20484a0e63e5fb768956b98d57b44be8f
lambert <lambert.torres@gmail.com>
2021-09-16 14:02:12 -0400

    added constructor

--- echo/lib/echo/game/eraser.ex
+++ echo/lib/echo/game/eraser.ex
@@ -1,6 +1,17 @@
 defmodule Echo.Game.Eraser do
+  defstruct [:phrase, :plan]
 
-  
+  def new(phrase, steps) do
+    len = String.length(phrase) - 1
+    step_size = ceil(len / steps)
+    plan = 0..len |> Enum.shuffle() |> Enum.chunk_every(step_size)
+    %__MODULE__{phrase: phrase, plan: plan}
+  end
+
+  def get_text(eraser) do
+    eraser.phrase
+  end
+
   def erase(%{phrase: phrase, plan: [masks | plan]} = game) do
     %{game | plan: plan, phrase: replace(phrase, masks)}
   end
@@ -8,12 +19,11 @@ defmodule Echo.Game.Eraser do
   # masked string for a given step
   defp replace(phrase, masks) do
     phrase
-    |> String.graphemes
-    |> Enum.with_index
-    |> Enum.map(fn {letter, index} -> 
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.map(fn {letter, index} ->
       if(Enum.member?(masks, index), do: "_", else: letter)
     end)
-    |> List.to_string
+    |> List.to_string()
   end
- 
 end
